0ce17edf41bb9f5d7a0f9cf8e74e7d244c307087
Revanth Srinivasa <revan@MacBook-Air.local>
2026-01-07 04:28:46 +0000

    Auth Fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-const BACKEND_URL = "http://localhost:5001";
+const BACKEND_URL = process.env.BACKEND_URL!;
 
 export async function POST(req: Request) {
   try {
